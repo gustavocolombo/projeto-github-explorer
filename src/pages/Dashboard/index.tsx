662e30894d77454b93662262ade0e124ea8b5b24
Gustavo Colombo <gusttavocolombo@gmail.com>
2020-08-07 23:40:30 -0300

    criando layout da lista de repositórios

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import logo from '../../assets/github_explorer.svg';
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
 
 const Dashboard: React.FC = () => {
   return (
@@ -13,6 +14,47 @@ const Dashboard: React.FC = () => {
         <input placeholder="Digite aqui o nome do repositório"/>
         <button type="submit">Pesquisar</button>
       </Form>
+
+      <Repositories>
+       <a href="teste">
+        <img src="https://avatars3.githubusercontent.com/u/66711378?s=460&u=1892f3e33fa834fb432991d37145567f42a6212c&v=4"
+          alt= "usuário do github"
+        />
+
+        <div>
+          <strong>gustavocolombo/ibuyonline</strong>
+          <p>Este repositório vai ser destinado a um projeto que estou desenvolvendo, um bazar virtual.</p>
+        </div>
+
+        <FiChevronRight size={20}/>
+       </a>
+
+       <a href="teste">
+        <img src="https://avatars3.githubusercontent.com/u/66711378?s=460&u=1892f3e33fa834fb432991d37145567f42a6212c&v=4"
+          alt= "usuário do github"
+        />
+
+        <div>
+          <strong>gustavocolombo/ibuyonline</strong>
+          <p>Este repositório vai ser destinado a um projeto que estou desenvolvendo, um bazar virtual.</p>
+        </div>
+
+        <FiChevronRight size={20}/>
+       </a>
+
+       <a href="teste">
+        <img src="https://avatars3.githubusercontent.com/u/66711378?s=460&u=1892f3e33fa834fb432991d37145567f42a6212c&v=4"
+          alt= "usuário do github"
+        />
+
+        <div>
+          <strong>gustavocolombo/ibuyonline</strong>
+          <p>Este repositório vai ser destinado a um projeto que estou desenvolvendo, um bazar virtual.</p>
+        </div>
+
+        <FiChevronRight size={20}/>
+       </a>
+      </Repositories>
     </>
   );
 
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -40,5 +40,57 @@ export const Form = styled.form`
       transition:0.3s;
     }
   }
+`;
 
+export const Repositories = styled.div`
+  margin-top:80px;
+  max-width:700px;
+
+  a{
+    background:#fff;
+    border-radius:5px;
+    width:100%;
+    padding:24px;
+    display:block;
+    text-decoration:none;
+
+    display:flex;
+    align-items:center;
+
+    &:hover{
+      transform: translateX(10px);
+      transition: 0.3s;
+    }
+
+    & + a{
+      margin-top:16px;
+    }
+
+  }
+
+  img{
+    width:64px;
+    height:64px;
+    border-radius:50%;
+  }
+
+  div{
+    margin-left:16px;
+
+      strong{
+        font-size:20px;
+        color:#3d3d4d;
+      }
+
+      p{
+        font-size:18px;
+        color:#A8A8B3;
+        margin-top:4px;
+      }
+
+      svg{
+        margin-left:auto;
+        color:#cbcbd6;
+      }
+  }
 `;
