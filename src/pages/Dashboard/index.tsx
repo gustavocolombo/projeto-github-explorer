a1c4045c8fd3b09ec483ab8be86a718fdf012ddf
Gustavo Colombo <gusttavocolombo@gmail.com>
2020-11-09 23:33:50 -0300

    listando repositórios da API do github

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,59 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 
 import logo from '../../assets/github_explorer.svg';
 import { Title, Form, Repositories } from './styles';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
+interface Repository{
+  full_name: string;
+  owner:{
+    login: string;
+    avatar_url: string;
+  };
+  description: string;
+}
 
 const Dashboard: React.FC = () => {
+
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [newRepo, setNewRepo] = useState('');
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>){
+    event.preventDefault();
+    const response = await api.get(`repos/${newRepo}`);
+
+    const repository  = response.data
+
+    setRepositories([...repositories, repository])
+  }
+
   return (
     <>
       <img src= {logo}  alt= "Github Explorer"/>
       <Title> Explore Repositórios no Github Explorer </Title>
 
-      <Form>
-        <input placeholder="Digite aqui o nome do repositório"/>
+      <Form onSubmit={handleAddRepository}>
+        <input
+        placeholder="Digite aqui o nome do repositório"
+        value={newRepo}
+        onChange={(e)=> setNewRepo(e.target.value)}/>
+
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-       <a href="teste">
-        <img src="https://avatars3.githubusercontent.com/u/66711378?s=460&u=1892f3e33fa834fb432991d37145567f42a6212c&v=4"
-          alt= "usuário do github"
-        />
+       {repositories.map( repository => {
+         return (
+          <a key={repository.full_name} href="teste">
+            <img
+            src={repository.owner.avatar_url}
+            alt={repository.owner.login}
+            />
 
-        <div>
-          <strong>gustavocolombo/ibuyonline</strong>
-          <p>Este repositório vai ser destinado a um projeto que estou desenvolvendo, um bazar virtual.</p>
-        </div>
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-        <FiChevronRight size={20}/>
-       </a>
+            <FiChevronRight size={20}/>
+          </a>
+        )
+       })}
 
-       <a href="teste">
-        <img src="https://avatars3.githubusercontent.com/u/66711378?s=460&u=1892f3e33fa834fb432991d37145567f42a6212c&v=4"
-          alt= "usuário do github"
-        />
-
-        <div>
-          <strong>gustavocolombo/ibuyonline</strong>
-          <p>Este repositório vai ser destinado a um projeto que estou desenvolvendo, um bazar virtual.</p>
-        </div>
-
-        <FiChevronRight size={20}/>
-       </a>
-
-       <a href="teste">
-        <img src="https://avatars3.githubusercontent.com/u/66711378?s=460&u=1892f3e33fa834fb432991d37145567f42a6212c&v=4"
-          alt= "usuário do github"
-        />
-
-        <div>
-          <strong>gustavocolombo/ibuyonline</strong>
-          <p>Este repositório vai ser destinado a um projeto que estou desenvolvendo, um bazar virtual.</p>
-        </div>
-
-        <FiChevronRight size={20}/>
-       </a>
       </Repositories>
     </>
   );
