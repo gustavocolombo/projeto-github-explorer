57091094820e12b8061b25aceda376e72190b712
Gustavo Colombo <gusttavocolombo@gmail.com>
2020-11-13 23:11:50 -0300

    adicionando header e rotas

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
-
+import { Link, useRouteMatch } from 'react-router-dom';
+import { Header } from '../Repository/styles';
+import logo from '../../assets/logo.svg';
+import { FiChevronLeft } from 'react-icons/fi';
 interface RepositoryParamas{
   repository: string;
 }
@@ -9,7 +11,16 @@ const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParamas>();
 
   return (
-    <h1>Repository: {params.repository}</h1>
+    <>
+      <h1>Repository: {params.repository}</h1>
+      <Header>
+          <img src={logo} alt="logo"/>
+          <Link to ="/">
+            <FiChevronLeft size={16}/>
+            Voltar para listagem
+          </Link>
+      </Header>
+    </>
   );
 }
 
