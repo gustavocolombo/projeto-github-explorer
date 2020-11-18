037f9e5f48b11c9ffcfabfdd3bb49866fb0d967d
Gustavo Colombo <gusttavocolombo@gmail.com>
2020-11-18 18:22:52 -0300

    fazendo requisições a API github

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useRouteMatch } from 'react-router-dom';
 import { Header, RepositoryInfo, Issues } from '../Repository/styles';
 import logo from '../../assets/logo.svg';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 interface RepositoryParamas{
   repository: string;
+}
+interface Repository{
+  full_name: string;
+  owner:{
+    login: string;
+    avatar_url: string;
+  };
+  description: string;
+  forks_count:number;
+  stargazers_count:number;
+  open_issues_count:number;
+}
+
+interface Issue{
+  id:number;
+  title:string;
+  user: {
+    login:string;
+  }
 }
 
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParamas>();
 
+  const[repositories, setRepositories] = useState<Repository | null>(null);
+  const[issues, setIssues] = useState<Issue[]>([]);
+
+  useEffect(()=> {
+    async function loadData(): Promise <void>{
+      const [repository, issues] = await Promise.all([
+        api.get(`repos/${params.repository}`),
+        api.get(`repos/${params.repository}/issues`)
+      ])
+    }
+  }, [params.repository]);
+
   return (
     <>
       <h1>Repository: {params.repository}</h1>
       <Header>
-          <img src={logo} alt="logo"/>
-          <Link to ="/">
-            <FiChevronLeft size={16}/>
-            Voltar para listagem
-          </Link>
+        <img src={logo} alt="logo"/>
+        <Link to ="/">
+          <FiChevronLeft size={16}/>
+          Voltar para listagem
+        </Link>
       </Header>
 
       <RepositoryInfo>
@@ -28,8 +60,8 @@ const Repository: React.FC = () => {
               <strong>Titulo</strong>
               <p>Descrição</p>
             </div>
-
         </header>
+
         <ul>
           <li>
             <strong>1808</strong>
