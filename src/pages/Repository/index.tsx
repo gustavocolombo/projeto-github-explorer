a79fffb8558624d4a53fd5d1e656b17082427849
Gustavo Colombo <gusttavocolombo@gmail.com>
2020-11-18 18:32:10 -0300

    mostrando dados dos repositórios

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -30,16 +30,19 @@ interface Issue{
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParamas>();
 
-  const[repositories, setRepositories] = useState<Repository | null>(null);
+  const[repository, setRepository] = useState<Repository | null>(null);
   const[issues, setIssues] = useState<Issue[]>([]);
 
   useEffect(()=> {
-    async function loadData(): Promise <void>{
-      const [repository, issues] = await Promise.all([
-        api.get(`repos/${params.repository}`),
-        api.get(`repos/${params.repository}/issues`)
-      ])
-    }
+
+    api.get(`repos/${params.repository}`).then(response=>{
+     setRepository(response.data);
+    });
+
+    api.get(`repos/${params.repository}/issues`).then(response=>{
+      setIssues(response.data);
+    })
+
   }, [params.repository]);
 
   return (
@@ -53,31 +56,33 @@ const Repository: React.FC = () => {
         </Link>
       </Header>
 
-      <RepositoryInfo>
+      {repository && (
+        <RepositoryInfo>
         <header>
-          <img src="https://avatars0.githubusercontent.com/u/28929274?s=200&v=4" alt=""/>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login}/>
             <div>
-              <strong>Titulo</strong>
-              <p>Descrição</p>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
             </div>
         </header>
 
         <ul>
           <li>
-            <strong>1808</strong>
+            <strong>{repository.stargazers_count}</strong>
             <span>Stars</span>
           </li>
           <li>
-            <strong>1808</strong>
+            <strong>{repository.forks_count}</strong>
             <span>Forks</span>
           </li>
           <li>
-            <strong>1808</strong>
+            <strong>{repository.open_issues_count}</strong>
             <span>Issues abertas</span>
           </li>
         </ul>
       </RepositoryInfo>
 
+      )}
       <Issues>
         <Link to="/">
 
